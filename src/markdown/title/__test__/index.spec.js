ff9fa3cb2dc6096a4f39e4e0298bc2bda59f785d
mqycn <mqycn@126.com>
2020-08-26 18:19:30 +0800

    完成 文件排序和缩进的功能，并编写测试代码

--- src/markdown/title/__test__/index.spec.js
+++ src/markdown/title/__test__/index.spec.js
@@ -1,18 +1,16 @@
 const path = require('path')
+const { withTitle, withTitles } = require('../index')
 
 function resolvePath(filePath) {
     return path.join(__dirname, filePath)
 }
 
-it('markdown 单文件测试', () => {
-    const { withTitle } = require('../index')
-
+it('markdown 单文件标题测试', () => {
     expect(withTitle('README.md', resolvePath).name)
         .toBe('会了吧')
 })
 
-it('markdown 多文件测试', () => {
-    const { withTitles } = require('../index')
+it('markdown 多文件标题测试', () => {
     const list = [
         'README.md',
         'help/README.md',
@@ -20,4 +18,40 @@ it('markdown 多文件测试', () => {
     ]
     expect(withTitles(list, resolvePath).map(item => item.name).join('; '))
         .toBe('会了吧; 会了吧使用教程; 会了吧 怎么下载')
+})
+
+it('markdown 多文件标题测试排序', () => {
+    const list = [
+        'help/1.md',
+        'help/README.md',
+        'README.md'
+    ]
+    expect(withTitles(list, resolvePath)
+        .map(item => item.name)
+        .join('; '))
+        .toBe('会了吧; 会了吧使用教程; 会了吧 怎么下载')
+})
+
+it('markdown 单文件标题 根目录 缩进测试', () => {
+    expect(withTitle('README.md', resolvePath).prefix)
+        .toBe('')
+})
+
+
+it('markdown 单文件标题 二级目录 缩进测试', () => {
+    expect(withTitle('help/README.md', resolvePath).prefix)
+        .toBe('　')
+})
+
+
+it('markdown 多文件标题 缩进测试', () => {
+    const list = [
+        'README.md',
+        'help/README.md',
+        'help/1.md'
+    ]
+    expect(withTitles(list, resolvePath)
+        .map(item => item.prefix)
+        .join('; '))
+        .toBe('; 　; 　')
 })
--- src/markdown/title/index.js
+++ src/markdown/title/index.js
@@ -1,7 +1,28 @@
-const fs = require('fs');
+const fs = require('fs')
+
+const README_FILE = 'README.md'
+
 
 // 批量获取标题
 function withTitles(paths, resolvePath) {
+
+  // 路径排序，README.md 放最前面
+  paths.sort((a, b) => {
+    if (a == README_FILE || b == README_FILE) {
+      //README.md放在最前面
+      return a == README_FILE ? -1 : 1
+    } else if (a.indexOf(README_FILE) > 0 && b.startsWith(a.substr(0, a.indexOf(README_FILE)))) {
+      //路径 a 中存在 README.md，并且路径前面相等
+      return -1
+    } else if (b.indexOf(README_FILE) > 0 && a.startsWith(b.substr(0, b.indexOf(README_FILE)))) {
+      //路径 b 中存在 README.md，并且路径前面相等
+      return 1
+    } else {
+      return a.localeCompare(b)
+    }
+  });
+
+  // 排序后分析标题
   return paths.map(path => withTitle(path, resolvePath))
 }
 
@@ -13,7 +34,8 @@ function withTitle(path, resolvePath) {
     }))
   return {
     path,
-    name: match ? match[1].trim() : path
+    name: match ? match[1].trim() : path,
+    prefix: path.split('/').map(item => '').join('　')
   }
 }
 
