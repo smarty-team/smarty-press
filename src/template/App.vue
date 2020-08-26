a23bca9563d5dd9be2d0eced57ef262edb7c1471
josephxia <15906475@qq.com>
2020-08-26 09:59:16 +0800

    adminlte add header

--- src/template/App.vue
+++ src/template/App.vue
@@ -46,6 +46,11 @@
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+
+      </div>
+      <!-- /.content-header -->
       <!-- Main content -->
       <div class="content">
         <div v-html="markdown"></div>
