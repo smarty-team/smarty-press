1693793bece07895b145d945a3044e3f287413cb
Qiyuan <mqycn@126.com>
2020-08-28 01:04:55 +0800

    增加 标题中间件

--- src/markdown/provider/index.js
+++ src/markdown/provider/index.js
@@ -1,2 +1,7 @@
-// Markdown Provider
-module.exports = require('Provider')
+const Provider = require('./Provider')
+
+const provider = new Provider() // Markdown Provider
+
+provider.useMiddleware(require('../title')) //解析标题
+
+module.exports = provider
