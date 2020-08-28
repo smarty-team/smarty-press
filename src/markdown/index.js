5ab628c9e5d89d0259da87265c5490907f385152
Qiyuan <mqycn@126.com>
2020-08-29 00:11:58 +0800

    themes 中间件路径写错了

--- src/markdown/index.js
+++ src/markdown/index.js
@@ -1,5 +1,5 @@
 module.exports = require('./provider')(
     require('./title'), // 解析标题
     require('./marked'),  // markdown转html
-    require('./styles')   // 添加样式
+    require('./themes')   // 添加样式
 )
