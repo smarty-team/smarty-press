67c9ee207b534cdcd37144824e5033a5b99f5df3
mqycn <mqycn@126.com>
2020-09-12 17:09:18 +0800

    markdown转html中间件，增加 菜单链接绑定

--- src/markdown/marked/index.js
+++ src/markdown/marked/index.js
@@ -1,7 +1,34 @@
 const marked = require('marked')
 
+let indexData = {}
+
 // Markdown 转 HTML
 module.exports = async ({ fileNode }, next) => {
-  fileNode.html = marked(fileNode.body)
   await next()
+
+  // 菜单处理
+  if (fileNode.catalogs instanceof Array) {
+    catelogs = {}
+    setCatelogs(fileNode.catalogs)
+  }
+
+  // HTML处理
+  fileNode.html = marked(fileNode.body)
+}
+
+function setCatelogs(catelogs) {
+  catelogs.forEach(item => {
+    if (item.hash.indexOf('#') == 0) {
+      const matched = marked(item.hash).match(/<h\d\sid="([^"]*)/)
+      if (matched) {
+        item.hash = matched[1]
+        let index = indexData[item.hash] || 0
+        indexData[item.hash] = ++index
+        if (index > 1) {
+          item.hash += '-' + (index - 1)
+        }
+      }
+    }
+    item.children && setCatelogs(item.children)
+  })
 }
