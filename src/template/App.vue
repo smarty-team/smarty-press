3cf434be139b6fe875dd32664b470ba1ef0ca174
Qiyuan <mqycn@126.com>
2020-10-04 16:18:15 +0800

    样式文件 移动到 assets/themes 目录

--- src/template/App.vue
+++ src/template/App.vue
@@ -1,5 +1,9 @@
 <template>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/assets/adminlte.min.css" />
+<link rel="stylesheet" :href="skinPath">
+
+
 <body class="hold-transition sidebar-mini layout-fixed">
   <div class="wrapper fixed">
     <div class="main-header"></div>
