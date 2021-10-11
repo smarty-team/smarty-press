5ec2f770e16fda1acceef668b1b7a147b327e23d
josephxia <15906475@qq.com>
2021-10-11 15:13:08 +0800

    update vue version

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 npm config get registry # 检查仓库镜像库
-npm config set registry=http://registry.npmjs.org
+npm config set registry=https://registry.npmjs.org
 echo '请进行登录相关操作：'
 npm login # 登陆
 echo "-------publishing-------"
